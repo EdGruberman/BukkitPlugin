81a377238e77b2bd65cf9c9bfc139b612f57ac69
EdGruberman <ed@rjump.com>
2011-06-14 11:47:01 -0700

    FIX: ConfigurationManager code clean-up.

--- src/edgruberman/bukkit/simpletemplate/ConfigurationManager.java
+++ src/edgruberman/bukkit/simpletemplate/ConfigurationManager.java
@@ -10,40 +10,47 @@ import java.util.GregorianCalendar;
 
 import org.bukkit.plugin.Plugin;
 
+/**
+ * Standardized plugin configuration file management class.<br />
+ * <br />
+ * Defaults will be extracted from the JAR.  Save requests can be cached to
+ * avoid performance penalties for too many save requests occurring too
+ * frequently.
+ * 
+ * @author EdGruberman
+ */
 public class ConfigurationManager {
     
     private static final int TICKS_PER_SECOND = 20;
     
     // Name of configuration file. (Used for both default supplied in JAR and the active one in the file system.)
-    private String file = "config.yml";
+    private static final String FILE = "config.yml";
     
     // Path to default configuration file supplied in JAR.
-    private String defaultPath = "/defaults/" + this.file;
-    
-    // Shortest duration in seconds a configuration file save can occur in for non-forced save requests.
-    private int maxSave = 10;
-    
-    private Calendar lastSave = null;
-    private Integer taskSave = null;
+    private static final String DEFAULT_PATH = "/defaults/" + ConfigurationManager.FILE;
     
     private Plugin plugin;
-    
+    private Calendar lastSave = null;
+    private Integer taskSave = null;
+    private int maxSaveFrequency = 10; // Shortest duration in seconds each configuration file save can occur in.
+
     protected ConfigurationManager(Plugin plugin) {
         this.plugin = plugin;
     }
     
     /**
-     * Create configuration file from default if necessary and then load.
+     * Loads the configuration file from plugin data folder.  This method will
+     * create the file from the default supplied in the JAR if necessary.
      * 
      * @param plugin Plugin to load configuration for.
      */
     protected void load() {
-        File fileConfig = new File(this.plugin.getDataFolder(), this.file);
+        File fileConfig = new File(this.plugin.getDataFolder(), ConfigurationManager.FILE);
         if (!fileConfig.exists()) {
             try {
-                this.extract(this.plugin.getClass().getResource(this.defaultPath), fileConfig);
+                this.extract(this.plugin.getClass().getResource(ConfigurationManager.DEFAULT_PATH), fileConfig);
             } catch (Exception e) {
-                System.err.println("Unable to extract default configuration file.");
+                System.err.println("[" + this.plugin.getDescription().getName() + "] Unable to extract default configuration file.");
                 e.printStackTrace();
             }
         }
@@ -73,36 +80,48 @@ public class ConfigurationManager {
         out.close();
     }
     
+    protected int getMaxSaveFrequency() {
+        return this.maxSaveFrequency;
+    }
+    
+    protected void setMaxSaveFrequency(int frequency) {
+        this.maxSaveFrequency = frequency;
+    }
+    
     /**
-     * Force a save of the configuration file immediately.
+     * Save the configuration file immediately. All cached save requests will be
+     * saved to the file system.
      */
     protected void save() {
         this.save(true);
     }
     
     /**
-     * Save the configuration file
+     * Request a save of the configuration file. If request is not required to
+     * be done immediately and last save was less than configured max frequency
+     * then request will be cached and a scheduled task will kick off after the
+     * max frequency has expired since last save.
      * 
-     * @param force true to force a save of the configuration file immediately.
+     * @param immediately true to force a save of the configuration file immediately.
      */
-    protected void save(boolean force) {
-        if (!force) {
+    protected void save(boolean immediately) {
+        if (!immediately) {
             // Determine how long since last save.
-            long lastSave = -1;
+            long sinceLastSave = this.maxSaveFrequency;
             if (this.lastSave != null)
-                lastSave = (System.currentTimeMillis() - this.lastSave.getTimeInMillis()) / 1000;
+                sinceLastSave = (System.currentTimeMillis() - this.lastSave.getTimeInMillis()) / 1000;
             
-            // Schedule task to run if last save was less than MAX_SAVE.
-            if (lastSave >= 0 && lastSave < this.maxSave) {
-                // If task already scheduled return and let currently scheduled task run when expected.
-                if (this.plugin.getServer().getScheduler().isQueued(this.taskSave)) return;
+            // Schedule a cache flush to run if last save was less than maximum save frequency.
+            if (sinceLastSave < this.maxSaveFrequency) {
+                // If task already scheduled let it run when expected.
+                if (this.taskSave != null && this.plugin.getServer().getScheduler().isQueued(this.taskSave)) return;
                 
-                // Schedule task to force save.
+                // Schedule task to save cache to file system.
                 final ConfigurationManager configurationManager = this;
                 this.taskSave = this.plugin.getServer().getScheduler().scheduleSyncDelayedTask(
                           this.plugin
                         , new Runnable() { public void run() { configurationManager.save(true); } }
-                        , (this.maxSave - lastSave) * ConfigurationManager.TICKS_PER_SECOND
+                        , (this.maxSaveFrequency - sinceLastSave) * ConfigurationManager.TICKS_PER_SECOND
                 );
             
                 return;
