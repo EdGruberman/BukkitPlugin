2e94a8c2446d73bb7598a4fd8956dcb5580164a9
EdGruberman <ed@rjump.com>
2011-07-25 18:16:28 -0700

    CHG: Automatically load configuration file on instantiation. CHG: Reduce save visibility to package default. Wrap in public implementation if necessary. CHG: Create parent folders of configuration file if needed.

--- src/edgruberman/bukkit/simpletemplate/Main.java
+++ src/edgruberman/bukkit/simpletemplate/Main.java
@@ -9,7 +9,6 @@ public final class Main extends org.bukkit.plugin.java.JavaPlugin {
     
     public void onLoad() {
         Main.configurationFile = new ConfigurationFile(this);
-        Main.configurationFile.load();
         
         Main.messageManager = new MessageManager(this);
         Main.messageManager.log("Version " + this.getDescription().getVersion());
