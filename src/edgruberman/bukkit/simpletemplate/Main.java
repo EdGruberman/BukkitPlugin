42cdeae1c70d8c7c81810f1d83a6d8cbcb835173
EdGruberman <ed@rjump.com>
2012-03-24 11:30:37 -0700

    Move logging level management into ConfigurationFile

--- src/edgruberman/bukkit/simpletemplate/Main.java
+++ src/edgruberman/bukkit/simpletemplate/Main.java
@@ -1,8 +1,5 @@
 package edgruberman.bukkit.simpletemplate;
 
-import java.util.logging.Handler;
-import java.util.logging.Level;
-
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -25,7 +22,7 @@ public final class Main extends JavaPlugin {
         this.configurationFile = new ConfigurationFile(this);
         this.configurationFile.setMinVersion(Main.MINIMUM_VERSION_CONFIG);
         this.configurationFile.load();
-        this.setLoggingLevel();
+        this.configurationFile.setLoggingLevel();
 
         new Message(this);
 
@@ -38,22 +35,6 @@ public final class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         if (this.configurationFile.isSaveQueued()) this.configurationFile.save();
-    }
-
-    private void setLoggingLevel() {
-        final String name = this.configurationFile.getConfig().getString("logLevel", "INFO");
-        Level level;
-        try { level = Level.parse(name); } catch (final Exception e) {
-            level = Level.INFO;
-            this.getLogger().warning("Unrecognized java.util.logging.Level in \"" + this.configurationFile.getFile().getPath() + "\"; logLevel: " + name);
-        }
-
-        // Only set the parent handler lower if necessary, otherwise leave it alone for other configurations that have set it.
-        for (final Handler h : this.getLogger().getParent().getHandlers())
-            if (h.getLevel().intValue() > level.intValue()) h.setLevel(level);
-
-        this.getLogger().setLevel(level);
-        this.getLogger().log(Level.CONFIG, "Logging level set to: " + this.getLogger().getLevel());
     }
 
     public void configure() {
