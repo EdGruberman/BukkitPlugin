0ea187a4d05ea4e378bd0c052e099f4f806b3624
EdGruberman <ed@rjump.com>
2012-08-15 13:40:12 -0700

    Added custom configuration loading method

--- src/edgruberman/bukkit/simpletemplate/Main.java
+++ src/edgruberman/bukkit/simpletemplate/Main.java
@@ -9,10 +9,13 @@ import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
 import java.nio.charset.Charset;
+import java.text.MessageFormat;
 import java.util.Date;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import edgruberman.bukkit.simpletemplate.commands.Reload;
@@ -42,16 +45,9 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void reloadConfig() {
-        this.saveDefaultConfig();
+        this.loadConfig("config.yml", Main.MINIMUM_CONFIGURATION);
         super.reloadConfig();
         this.setLogLevel(this.getConfig().getString("logLevel"));
-
-        final Version version = new Version(this.getConfig().isSet("version") ? this.getConfig().getString("version") : null);
-        if (version.compareTo(Main.MINIMUM_CONFIGURATION) >= 0) return;
-
-        this.archiveConfig("config.yml", version);
-        this.saveDefaultConfig();
-        this.reloadConfig();
     }
 
     @Override
@@ -59,15 +55,22 @@ public final class Main extends JavaPlugin {
         this.extractConfig("config.yml", false);
     }
 
-    private void archiveConfig(final String resource, final Version version) {
-        final String backupName = "%1$s - Archive version %2$s - %3$tY%3$tm%3$tdT%3$tH%3$tM%3$tS.yml";
-        final File backup = new File(this.getDataFolder(), String.format(backupName, resource.replaceAll("(?i)\\.yml$", ""), version, new Date()));
+    private Configuration loadConfig(final String resource, final Version required) {
+        // extract default if not existing
+        this.extractConfig(resource, false);
+
         final File existing = new File(this.getDataFolder(), resource);
+        final Configuration config = YamlConfiguration.loadConfiguration(existing);
+        if (required == null) return config;
 
-        if (!existing.renameTo(backup))
-            throw new IllegalStateException("Unable to archive configuration file \"" + existing.getPath() + "\" with version \"" + version + "\" to \"" + backup.getPath() + "\"");
+        // verify required or later version
+        final Version version = new Version(config.getString("version"));
+        if (version.compareTo(required) >= 0) return config;
 
-        this.getLogger().warning("Archived configuration file \"" + existing.getPath() + "\" with version \"" + version + "\" to \"" + backup.getPath() + "\"");
+        this.archiveConfig(resource, version);
+
+        // extract default and reload
+        return this.loadConfig(resource, null);
     }
 
     private void extractConfig(final String resource, final boolean replace) {
@@ -93,14 +96,25 @@ public final class Main extends JavaPlugin {
         }
     }
 
+    private void archiveConfig(final String resource, final Version version) {
+        final String backupName = "{0} - Archive version {1} - {2,date,yyyyMMdd}{2,time,HHmmss}.yml";
+        final File backup = new File(this.getDataFolder(), MessageFormat.format(backupName, resource.replaceAll("(?i)\\.yml$", ""), version, new Date()));
+        final File existing = new File(this.getDataFolder(), resource);
+
+        if (!existing.renameTo(backup))
+            throw new IllegalStateException("Unable to archive configuration file \"" + existing.getPath() + "\" with version \"" + version + "\" to \"" + backup.getPath() + "\"");
+
+        this.getLogger().warning("Archived configuration file \"" + existing.getPath() + "\" with version \"" + version + "\" to \"" + backup.getPath() + "\"");
+    }
+
     private void setLogLevel(final String name) {
         Level level;
         try { level = Level.parse(name); } catch (final Exception e) {
             level = Level.INFO;
-            this.getLogger().warning("Log level defaulted to " + level.getName() + "; Unrecognized java.util.logging.Level: " + name);
+            this.getLogger().warning("Log level defaulted to " + level.getName() + "; Unrecognized java.util.logging.Level: " + name + "; " + e);
         }
 
-        // Only set the parent handler lower if necessary, otherwise leave it alone for other configurations that have set it
+        // only set the parent handler lower if necessary, otherwise leave it alone for other configurations that have set it
         for (final Handler h : this.getLogger().getParent().getHandlers())
             if (h.getLevel().intValue() > level.intValue()) h.setLevel(level);
 
