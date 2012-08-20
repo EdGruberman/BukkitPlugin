a1e7bd8f1670a0dc07e642bf98a8ac44acb328a2
EdGruberman <ed@rjump.com>
2012-08-20 00:30:46 -0700

    Minor syntax change for onLoad

--- src/edgruberman/bukkit/simpletemplate/Main.java
+++ src/edgruberman/bukkit/simpletemplate/Main.java
@@ -10,9 +10,7 @@ public final class Main extends CustomPlugin {
     public static Courier courier;
 
     @Override
-    public void onLoad() {
-        this.putConfigMinimum("config.yml", "0.0.0a0");
-    }
+    public void onLoad() { this.putConfigMinimum("config.yml", "0.0.0a0"); }
 
     @Override
     public void onEnable() {
