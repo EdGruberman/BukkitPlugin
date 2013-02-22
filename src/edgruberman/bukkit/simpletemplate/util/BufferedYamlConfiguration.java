eeff0fbe8dd08a6e742beb8123f054f88f327633
EdGruberman <ed@rjump.com>
2013-02-21 22:41:59 -0700

    Reword from delayed to lazy

--- src/edgruberman/bukkit/simpletemplate/util/BufferedYamlConfiguration.java
+++ src/edgruberman/bukkit/simpletemplate/util/BufferedYamlConfiguration.java
@@ -17,7 +17,7 @@ import org.bukkit.plugin.Plugin;
  * Queues save requests to prevent occurring more than a maximum rate.  Also enables delayed serialization processing for individual sections to occur at time of save.
  *
  * @author EdGruberman (ed@rjump.com)
- * @version 1.2.0
+ * @version 1.3.0
  */
 public class BufferedYamlConfiguration extends YamlConfiguration implements Runnable {
 
@@ -26,7 +26,7 @@ public class BufferedYamlConfiguration extends YamlConfiguration implements Runn
     private final Plugin plugin;
     private File file;
     private final long rate;
-    private final Map<String, ConfigurationSerializable> delayedSections = new LinkedHashMap<String, ConfigurationSerializable>();
+    private final Map<String, ConfigurationSerializable> lazySections = new LinkedHashMap<String, ConfigurationSerializable>();
     private long lastSaveAttempt = -1;
     private int taskSave = -1;
 
@@ -84,9 +84,9 @@ public class BufferedYamlConfiguration extends YamlConfiguration implements Runn
     /** force immediate save */
     public void save() {
         try {
-            for (final Map.Entry<String, ConfigurationSerializable> section : this.delayedSections.entrySet())
+            for (final Map.Entry<String, ConfigurationSerializable> section : this.lazySections.entrySet())
                 this.set(section.getKey(), section.getValue().serialize());
-            this.delayedSections.clear();
+            this.lazySections.clear();
 
             super.save(this.file);
 
@@ -133,8 +133,8 @@ public class BufferedYamlConfiguration extends YamlConfiguration implements Runn
      * queue a ConfigurationSection to be updated when the next save occurs, request order preserved
      * @param path section path (a conflicting pending request will be replaced and put at the end of the queue)
      */
-    public void delaySection(final String path, final ConfigurationSerializable serializable) {
-        this.delayedSections.put(path, serializable);
+    public void setLazy(final String path, final ConfigurationSerializable section) {
+        this.lazySections.put(path, section);
     }
 
 }
