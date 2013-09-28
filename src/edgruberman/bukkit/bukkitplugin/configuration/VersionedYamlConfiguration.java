642304509e37a17830349cce0c58944d298b9956
EdGruberman <ed@rjump.com>
2013-09-27 21:17:24 -0700

    remove out of date javadocs

--- src/edgruberman/bukkit/bukkitplugin/configuration/VersionedYamlConfiguration.java
+++ src/edgruberman/bukkit/bukkitplugin/configuration/VersionedYamlConfiguration.java
@@ -12,18 +12,20 @@ import edgruberman.bukkit.bukkitplugin.versioning.Version;
 /**
  * stores version tag in header
  * and supports migration of existing configuration files
- * @version 1.0.0
+ * @version 1.0.1
  */
 public class VersionedYamlConfiguration extends YamlConfiguration {
 
     /** first capturing group is used to determine version */
-    public static final Pattern VERSION_PARSE = Pattern.compile("(?:^" + YamlConfiguration.COMMENT_PREFIX + "@version (" + StandardVersion.PARSE_PATTERN.pattern() + ")\\n)");
+    public static final Pattern VERSION_PARSE = Pattern.compile("(?:^" + YamlConfiguration.COMMENT_PREFIX + "@version (" + StandardVersion.PARSE_PATTERN.pattern() + ")\\r?\\n)");
 
     /** first capturing group is used to remove version tag */
     public static final Pattern VERSION_STRIP = Pattern.compile("(" + VersionedYamlConfiguration.VERSION_PARSE.pattern() + ")");
 
-    /** 0 = readable, 1 = built header */
-    public static final String VERSION_STAMP = "# @version {0}\n{1}";
+    /** 0 = readable, 1 = built header, 2 = line break */
+    public static final String VERSION_STAMP = "# @version {0}{2}{1}";
+
+    protected static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
 
 
@@ -59,16 +61,14 @@ public class VersionedYamlConfiguration extends YamlConfiguration {
         final String built = super.buildHeader();
         if (this.version == Version.MISSING) return built;
 
-        final String stamped = MessageFormat.format(VersionedYamlConfiguration.VERSION_STAMP, this.version.toReadable(), built);
+        final String stamped = MessageFormat.format(VersionedYamlConfiguration.VERSION_STAMP, this.version.toReadable(), built, VersionedYamlConfiguration.LINE_SEPARATOR);
         return stamped;
     }
 
-    /** temporarily strips version tag for comparison */
     public boolean conflicts(final ConfigurationDefinition definition) {
         return definition.conflicts(this, this.getVersion());
     }
 
-    /** temporarily strips version tag for comparison */
     public void apply(final ConfigurationDefinition definition) {
         definition.apply(this, this.getVersion());
         this.setVersion(definition.getLatest());
